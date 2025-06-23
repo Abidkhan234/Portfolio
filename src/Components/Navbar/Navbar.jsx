44e5ac1ec8e5da04e7f3390fddf4e01903263cbc
AbidKhan234 <abidshahkhan237@gmail.com>
2025-06-23 12:19:02 +0500

    fix: update CV import paths in HeroContent, Navbar, and Sidebar components

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import Sidebar from "../Sidebar/Sidebar";
 import Button from "../Button/Button";
 import { LuDownload } from "react-icons/lu";
 import { useAppContext } from "../../../Context/Context";
+import CV from "../../../.My-CV/New-CV.pdf";
 
 const Navbar = () => {
   const { toggleBtn, setToggleBtn, navLinks } = useAppContext();
@@ -50,7 +51,7 @@ const Navbar = () => {
             data-aos-delay={`${(500 * 4) / 2}`}
             data-aos-easing="ease-in-out"
           >
-            <a href={"../../../.My-CV/New-CV.pdf"} download="Abid-Khan-CV.pdf">
+            <a href={CV} download="Abid-Khan-CV.pdf">
               <Button text={"CV"} icon={<LuDownload />} />
             </a>
           </div>
